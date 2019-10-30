bf7c1d8d7cb626781f287ba813436127cfc29764
PC <sith114@wp.pl>
2019-10-30 20:22:14 +0100

    Initial commit

---
+++ Api_download.py
@@ -0,0 +1,19 @@
+import lxml.html
+import requests
+import json
+from lxml import etree
+
+title="Taken"
+web="http://www.omdbapi.com/?t={}&apikey=55f1c482".format(title)
+#page = requests.get("http://www.omdbapi.com/?t=Taken&apikey=55f1c482").text
+page = requests.get(web).text
+print(page)
+
+print(type(page))
+
+
+x = json.loads(page)
+print(x)
+print(type(x))
+print(x["Year"])
+print(x["Title"])
