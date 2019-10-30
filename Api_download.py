2fe5dfd25aa34613d5eec17a6db4a02957ccf73c
PC <sith114@wp.pl>
2019-10-30 20:43:23 +0100

    30.10 starting

--- Api_download.py
+++ Api_download.py
@@ -4,18 +4,19 @@ import json
 from lxml import etree
 
 title="Taken"
-web="http://www.omdbapi.com/?t={}&apikey=55f1c482".format(title)
-#page = requests.get("http://www.omdbapi.com/?t=Taken&apikey=55f1c482").text
-page = requests.get(web).text
-print(page)
-
-print(type(page))
 
 
-x = json.loads(page)
-print(x)
-print(type(x))
-print(x["Year"])
-print(x["Title"])
 
-#a
+class Omdb():
+    def dict_create(self,title):
+        web = "http://www.omdbapi.com/?t={}&apikey=55f1c482".format(title)
+        page = requests.get(web).text
+        page = json.loads(page)
+        print(page)
+        print(type(page))
+
+o=Omdb()
+o.dict_create(title)
+
+
+
