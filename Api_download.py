99f1de809db2826e3611b0656def220620702829
PC <sith114@wp.pl>
2019-10-31 02:29:54 +0100

    31.10 Objective ver1

--- Api_download.py
+++ Api_download.py
@@ -6,36 +6,68 @@ import numpy as np
 from lxml import etree
 
 #title="Taken"
+class File():
 
-class Open_file():
     def open():
         data_frame = pd.read_csv("movies.csv")
+        #print(data_frame)
         return data_frame
 
-class Save_file():
-    def save(self):
-        self.to_csv("movies.csv", index=False)
+    def save2():
+        data_frame.to_csv("movies_updated.csv", index=False)
 
-class Omdb():
+    def save(self):
+        self.to_csv("movies_updated.csv", index=False)
+
+class Movie_update():
+    movie_title=" "
+
+    def title(self,data, ite):
+        row = data.values[ite]
+        #print(row)
+        print(row[1])
+        self.movie_title=row[1]
+        return self.movie_title
+
+    def dict_create(self):
+        web = "http://www.omdbapi.com/?t={}&apikey=55f1c482".format(self.movie_title)
+        page = requests.get(web).text
+        page = json.loads(page)
+        print(page)
+        #print(type(page))
+        return page
+
+###########################################################################
+'''class Omdb():
     def dict_create(self):
         web = "http://www.omdbapi.com/?t={}&apikey=55f1c482".format(self)
         page = requests.get(web).text
         page = json.loads(page)
         print(page)
-        print(type(page))
-
-class Get_title():
-    def title(self):
-        row = self.values[0]
+        #print(type(page))
+        return page'''
+'''class Get_title():
+    def title(self,ite):
+        row = self.values[ite]
+        #print(row)
         #print(row[1])
-        return row[1]
-
+        return row[1]'''
+class Add_Year():
+    pass
+############################################################################
 #data_frame.shape[0] - ilośc wierszy
 
-data_frame = Open_file.open()
-print(data_frame.shape[0])
-x=Get_title.title(data_frame)
-print("Movie title:",x)
-Omdb.dict_create(x)
+#-----------------------------------------------------------
+data_frame = File.open()
 
-Save_file.save(data_frame)
+
+i=0
+movie_list_update = Movie_update()
+while i < data_frame.shape[0]:
+    ite = i
+    movie_list_update.title(data_frame,i)
+    movie_list_update.dict_create()
+    i+=1
+
+File.save(data_frame)
+print("END")
