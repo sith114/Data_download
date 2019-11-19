0f9adc123ce7ac6bc6c1b19d270ed666616cf903
PC <sith114@wp.pl>
2019-11-20 00:47:57 +0100

    Script_start Ready to send 20.11

--- Readme.txt
+++ Readme.txt
@@ -2,7 +2,9 @@ Movie base, before first use downlat data by --data!
 
 optional arguments:
   -h, --help            show this help message and exit
+
   -d, --data            Download movies data
+
   -s1 SORT_BY1, --sort_by1 SORT_BY1
                         Sort by one: title,year,runtime,genre,
                         imdb_rating,box_office
@@ -10,12 +12,15 @@ optional arguments:
   -s2, --sort_by2       To sort put True as an argument Sort by two: Sort
                         movie form a to z in each year (Will add more
                         optrions)
+
   -f, --filtr           Filtr movies that earnd more than 100,000,000$ )
+
   -c COMP [COMP ...], --comp COMP [COMP ...]
                         Type of compare, movie1, movie2, movie3... type of
-                        comape is: runtime, Box office, IMDb Rating, awards
-                        won
+                        comape is: runtime, box_office, imdb_rating, awards
+
   -a ADD, --add ADD     Add movie, give movie title
+
   -hi, --highscore      Show highscore in some categories
 
   To first use we only need	movies.csv!
--- Script_start.py
+++ Script_start.py
@@ -42,6 +42,109 @@ def Box_clear(a):
         a=0
     return a
 
+def Add_Movie():
+    data_frame = Api_download.File.open("movies_updated2.csv")
+    movie_list_update = Api_download.Movie_update
+    movie_list_update.add_title(data_frame, args.add)
+    movie_list_update.data_row(movie_list_update, data_frame, data_frame.index.max())
+    movie_list_update.title(movie_list_update, data_frame, data_frame.index.max())
+    movie_list_update.dict_create(movie_list_update)
+    movie_list_update.add_movie(movie_list_update)
+    movie_list_update.update_data_frame(movie_list_update, data_frame, data_frame.index.max())
+    print(data_frame)
+    Api_download.File.save2(data_frame)
+
+def Highscore():
+    print("highscore is:")
+    #runtime===================================
+    runtime_var=runtime()
+    runtime_var=runtime()["runtime"][runtime_var.shape[0]-1]
+    runtime_var_movie=runtime()["title"][runtime().shape[0]-1]
+    print("Runtime={}".format(runtime_var),"Title of movie is {}".format(runtime_var_movie))
+
+    #IMBd rating==================================
+    data_frame = Api_download.File.open("movies_updated2.csv")
+    data_frame = data_frame.sort_values(by=['imdb_rating']).reset_index(drop=True)
+    print("IMBd rating={}".format(data_frame["imdb_rating"][runtime().shape[0]-1]),"Title of movie is {}".format(data_frame["title"][runtime().shape[0]-1]))
+    # box_office==================================
+    print("Box Office={}$".format(box_office()["box_office"][box_office().shape[0]-1]),"Title of movie is {}".format(box_office()["title"][box_office().shape[0]-1]))
+def Compare_awards_won():
+    for i in range(0, data_frame.shape[0]):
+        x = data_frame.values[i][1]
+        # print(x)
+        if x == args.comp[1]:
+            awards1 = data_frame.values[i][10]
+
+        elif x == args.comp[2]:
+            awards2 = data_frame.values[i][10]
+    print(args.comp[1], awards1)
+    print(args.comp[2], awards2)
+def Compare_imbd():
+    # print("IMDb Rating")
+    for i in range(0, data_frame.shape[0]):
+        x = data_frame.values[i][1]
+        # print(x)
+        if x == args.comp[1]:
+            imdb1 = data_frame.values[i][11]
+
+        elif x == args.comp[2]:
+            imdb2 = data_frame.values[i][11]
+    # print(imdb1,imdb2)
+    # print(type(imdb2))
+    if imdb1 > imdb2:
+        print(args.comp[1], imdb1, 'has bigger IMDb Rating than', args.comp[2], imdb2)
+    elif imdb2 > imdb1:
+        print(args.comp[2], imdb2, 'has bigger IMDb Rating than', args.comp[1], imdb1)
+
+def Compare_box_office():
+    # print("Box")
+    for i in range(0, data_frame.shape[0]):
+        x = data_frame.values[i][1]
+        # print(x)
+        if x == args.comp[1]:
+            box1 = data_frame.values[i][13]
+
+        elif x == args.comp[2]:
+            box2 = data_frame.values[i][13]
+    box1 = Box_clear(box1)
+    box2 = Box_clear(box2)
+
+    if box1 > box2:
+        print(args.comp[1], box1, 'has bigger box office than', args.comp[2], box2)
+    elif box2 > box1:
+        print(args.comp[2], box2, 'has bigger box office than', args.comp[1], box1)
+
+def Compare_runtime():
+    # print(data_frame["title"].str.find(args.comp[1],0).to_string())
+    for i in range(0, data_frame.shape[0]):
+        x = data_frame.values[i][1]
+        # print(x)
+        if x == args.comp[1]:
+            run1 = data_frame.values[i][3]
+            run1 = int(run1.split(" ")[0])
+
+        elif x == args.comp[2]:
+            run2 = data_frame.values[i][3]
+            run2 = int(run2.split(" ")[0])
+
+    if run1 > run2:
+        print(args.comp[1], run1, 'is longer than', args.comp[2], run2)
+    elif run2 > run1:
+        print(args.comp[2], run2, 'is longer than', args.comp[1], run1)
+def Filtering():
+    #print('Filtrujemy')
+    #print(box_office())
+    data_frame_box=box_office()
+
+    for i in range(0, box_office().shape[0]):
+        x = box_office().values[i][1]
+        if x>100000000:
+            data_frame_box.loc[i , 'box_office'] = x
+            #print("dodaje")
+        else:
+            data_frame_box=data_frame_box.drop(i)
+    print(data_frame_box.reset_index(drop = True))
+
 
 parser = argparse.ArgumentParser(description='Movie base, before first use downlat data by --data!')
 
@@ -99,20 +202,10 @@ if args.sort_by2 is not None:
         data_frame = Api_download.File.open("movies_updated2.csv")
         data_frame = data_frame.sort_values(by=["year","title"]).reset_index(drop = True)
         print(data_frame[["year","title"]].to_string())
+
 #filtr
 if args.filtr is not None:
-    print('Filtrujemy')
-    #print(box_office())
-    data_frame_box=box_office()
-
-    for i in range(0, box_office().shape[0]):
-        x = box_office().values[i][1]
-        if x>100000000:
-            data_frame_box.loc[i , 'box_office'] = x
-            #print("dodaje")
-        else:
-            data_frame_box=data_frame_box.drop(i)
-    print(data_frame_box.reset_index(drop = True))
+    Filtering()
 
 
 #Compare====================================================================
@@ -122,104 +215,24 @@ if args.comp is not None:
 
     #Runtime================================================================
     if args.comp[0]=="runtime":
-        #print(data_frame["title"].str.find(args.comp[1],0).to_string())
-        for i in range(0, data_frame.shape[0]):
-            x = data_frame.values[i][1]
-            #print(x)
-            if x == args.comp[1]:
-                run1=data_frame.values[i][3]
-                run1=int(run1.split(" ")[0])
+        Compare_runtime()
 
-            elif x==args.comp[2]:
-                run2=data_frame.values[i][3]
-                run2=int(run2.split(" ")[0])
-
-        if run1>run2:
-            print(args.comp[1],run1,'is longer than',args.comp[2],run2)
-        elif run2>run1:
-            print(args.comp[2], run2, 'is longer than', args.comp[1], run1)
     #Box_Office=========================================================
-    if args.comp[0]=="Box office":
-        #print("Box")
-        for i in range(0, data_frame.shape[0]):
-            x = data_frame.values[i][1]
-            #print(x)
-            if x == args.comp[1]:
-                box1=data_frame.values[i][13]
+    if args.comp[0]=="box_office":
+        Compare_box_office()
 
-            elif x==args.comp[2]:
-                box2=data_frame.values[i][13]
-        box1 = Box_clear(box1)
-        box2 = Box_clear(box2)
-
-        if box1 > box2:
-            print(args.comp[1],box1,'has bigger box office than',args.comp[2],box2)
-        elif box2>box1:
-            print(args.comp[2], box2, 'has bigger box office than', args.comp[1], box1)
     # IMDb Rating=========================================================
-    if args.comp[0]=="IMDb Rating":
-        #print("IMDb Rating")
-        for i in range(0, data_frame.shape[0]):
-            x = data_frame.values[i][1]
-            #print(x)
-            if x == args.comp[1]:
-                imdb1=data_frame.values[i][11]
+    if args.comp[0]=="imdb_rating":
+        Compare_imbd()
 
-            elif x==args.comp[2]:
-                imdb2=data_frame.values[i][11]
-        #print(imdb1,imdb2)
-        #print(type(imdb2))
-        if imdb1 > imdb2:
-            print(args.comp[1],imdb1,'has bigger IMDb Rating than',args.comp[2],imdb2)
-        elif imdb2>imdb1:
-            print(args.comp[2], imdb2, 'has bigger IMDb Rating than', args.comp[1], imdb1)
     # awards won=========================================================
-    if args.comp[0]=="awards won":
-        #print("awards won")
-        for i in range(0, data_frame.shape[0]):
-            x = data_frame.values[i][1]
-            #print(x)
-            if x == args.comp[1]:
-                awards1=data_frame.values[i][10]
-
-            elif x==args.comp[2]:
-                awards2=data_frame.values[i][10]
-        print(args.comp[1],awards1)
-        print(args.comp[2],awards2)
-
-
-
+    if args.comp[0]=="awards":
+        Compare_awards_won()
 
 #Dodanie do pliku================================================================
 if args.add is not None:
-    #print(Api_download.data_frame)
-    #print(Api_download.File.open("movies_updated2.csv"))
-    data_frame = Api_download.File.open("movies_updated2.csv")
-    movie_list_update = Api_download.Movie_update
-    movie_list_update.add_title(data_frame,args.add)
-    movie_list_update.data_row(movie_list_update,data_frame, data_frame.index.max())
-    movie_list_update.title(movie_list_update,data_frame, data_frame.index.max())
-    movie_list_update.dict_create(movie_list_update)
-    movie_list_update.add_movie(movie_list_update)
-    movie_list_update.update_data_frame(movie_list_update,data_frame, data_frame.index.max())
-    print(data_frame)
-    Api_download.File.save2(data_frame)
+    Add_Movie()
 
 if args.highscore is not None:
-    print("highscore is:")
-    #runtime===================================
-    runtime_var=runtime()
-    runtime_var=runtime()["runtime"][runtime_var.shape[0]-1]
-    runtime_var_movie=runtime()["title"][runtime().shape[0]-1]
-    print("Runtime={}".format(runtime_var),"Title of movie is {}".format(runtime_var_movie))
-
-    #IMBd rating==================================
-    data_frame = Api_download.File.open("movies_updated2.csv")
-    data_frame = data_frame.sort_values(by=['imdb_rating']).reset_index(drop=True)
-    print("IMBd rating={}".format(data_frame["imdb_rating"][runtime().shape[0]-1]),"Title of movie is {}".format(data_frame["title"][runtime().shape[0]-1]))
-    # box_office==================================
-    print("Box Office={}".format(box_office()["box_office"][box_office().shape[0]-1]),"Title of movie is {}".format(box_office()["title"][box_office().shape[0]-1]))
-
-
-
+    Highscore()
 
